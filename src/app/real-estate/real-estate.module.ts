f6be656f28733fa2b15964cb6754dd9b99f8d62d
Gabriel A. López López <glpz@daxslab.com>
2020-04-09 18:18:17 -0400

    NEW: gallery

--- src/app/real-estate/real-estate.module.ts
+++ src/app/real-estate/real-estate.module.ts
@@ -1,20 +1,23 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import { NgxGalleryModule } from 'ngx-gallery';
 
-import { IonicModule } from '@ionic/angular';
+import {IonicModule} from '@ionic/angular';
 
-import { RealEstatePageRoutingModule } from './real-estate-routing.module';
+import {RealEstatePageRoutingModule} from './real-estate-routing.module';
 
-import { RealEstatePage } from './real-estate.page';
+import {RealEstatePage} from './real-estate.page';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    RealEstatePageRoutingModule
-  ],
-  declarations: [RealEstatePage]
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        NgxGalleryModule,
+        RealEstatePageRoutingModule
+    ],
+    declarations: [RealEstatePage]
 })
-export class RealEstatePageModule {}
+export class RealEstatePageModule {
+}
--- src/app/real-estate/real-estate.page.ts
+++ src/app/real-estate/real-estate.page.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
+import {NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation} from 'ngx-gallery';
 import {RealEstateService} from "./../shared/real-estate.service";
 import {RealEstate} from "./../shared/real-estate";
 
@@ -13,6 +14,27 @@ export class RealEstatePage implements OnInit {
     private id: string;
     model: RealEstate;
 
+    galleryOptions: NgxGalleryOptions[] = [
+        {
+            "image": false,
+            "thumbnailsRemainingCount": true,
+            "width": "100%",
+            "height": "500px",
+            "thumbnailsColumns": 3,
+            "thumbnailsRows": 3,
+            "previewCloseOnClick": true,
+            "previewCloseOnEsc": true,
+            "previewKeyboardNavigation": true,
+            "previewArrows": true,
+        },
+        {
+            "breakpoint": 768,
+            "thumbnailsColumns": 1,
+            "thumbnailsRows": 4,
+        },
+    ];
+    images: NgxGalleryImage[] = [];
+
     constructor(private activatedRoute: ActivatedRoute, private realEstateService: RealEstateService) {
     }
 
@@ -21,9 +43,25 @@ export class RealEstatePage implements OnInit {
     }
 
     ionViewWillEnter() {
+
+
         this.realEstateService.getRealEstate(this.id).subscribe((res) => {
             console.log(res)
             this.model = res;
+
+            let page = this;
+            this.model.images.forEach(function (item) {
+                page.images.push({
+                    // 'altText': 'example',
+                    // 'url': item.url,
+                    // 'extUrl': item.url,
+                    // 'thumbnailUrl': item.url
+                    'small': item.url,
+                    'medium': item.url,
+                    'big': item.url
+                });
+            });
+            console.log(page, page.images);
         })
     }
 
