92d210c4eecc7d9e60aaee5175ae723a686ced24
Gabriel A. López López <glpz@daxslab.com>
2020-04-09 12:41:09 -0400

    NEW: real estate list and view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,28 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'folder/Inbox',
-    pathMatch: 'full'
-  },
-  {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
-  }
+    {
+        path: '',
+        redirectTo: 'real-estate',
+        pathMatch: 'full'
+    },
+    {
+        path: 'real-estate',
+        loadChildren: () => import('./real-estate-list/real-estate-list.module').then(m => m.RealEstateListPageModule)
+    },
+    {
+        path: 'real-estate/:id',
+        loadChildren: () => import('./real-estate/real-estate.module').then(m => m.RealEstatePageModule)
+    }
+
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
+    imports: [
+        RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules})
+    ],
+    exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+}
---
+++ src/app/shared/real-estate.ts
@@ -0,0 +1,5 @@
+export class RealEstate {
+    id: number;
+    title: string;
+    description: string;
+}
